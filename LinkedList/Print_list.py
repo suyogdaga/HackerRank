063d5d83ceefecc361ffb4db671a3a2510ef19c0
suyogdaga <suyogdaga@gmail.com>
2017-01-28 18:17:12 -0500

    Printing a LinkedList

---
+++ LinkedList/Print_list.py
@@ -0,0 +1,24 @@
+"""
+ Print elements of a linked list on console
+ head input could be None as well for empty list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+ 
+ 
+"""
+
+def print_list(head):
+    if head.next== None:
+        return
+    else:
+        temp = head
+        while temp.next!=None:
+            print temp.data
+            temp = temp.next
+        print temp.data
+        return
